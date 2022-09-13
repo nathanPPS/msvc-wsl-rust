e505d02c4393cd34e8c7e5540962a846a6e51c18
Nathan Larsen <nathan@PPS-NATHAN2.localdomain>
2022-09-13 13:07:50 -0500

    My configuration

--- config.sh
+++ config.sh
@@ -2,15 +2,15 @@
 
 # Name of WSL distro
 # Find it in "\\wsl$\"
-wsl_distro=Arch
+wsl_distro="Ubuntu-22.04"
 
 # Version of Visual Studio you installed
-vs_version=2019
+vs_version=2022
 
 # Version of MSVC tools
 # Find it in "C:\Program Files (x86)\Microsoft Visual Studio\$vs_version\BuildTools\VC\Tools\MSVC"
-tools_version=14.21.27702
+tools_version=14.33.31629
 
 # Version of Windows 10 SDK
 # Find it in "C:\Program Files (x86)\Windows Kits\10\Lib"
-sdk_version=10.0.17763.0
+sdk_version=10.0.19041.0
